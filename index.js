8e3a2f6d157d627dc60e283fc48035b5c95b97c1
isacvale <isac@ocastudios.com>
2019-02-11 23:58:40 -0200

    Added unsubscribe code. It's still crude, relying on loops and ifs, and should be refactored to use high-order functions.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 export default {
+  sub: new Map(),
   setSetter: function( target, prop, cbkFunc ){
+    Object.defineProperty( target, `_HuginnAndMuninnAreWatching${prop}`, {
+      enumerable: false
+    })
     Object.defineProperty( target, prop, {
       set: value => {
         this.sub.get(target)[prop].forEach( func => func() )
@@ -17,5 +21,31 @@ export default {
     this.sub.get( target )[prop].push( cbkFunc )
     this.setSetter( target, prop, cbkFunc )
   },
-  sub: new Map()
+  unsubscribe: function(){
+    return function ( obj = "_all", prop = "_all", func = "_all" ) {
+      for( let objRef of this.sub.keys() ){
+        if( obj == objRef || obj == "_all" ){
+          for( let propName of Object.keys( this.sub.get(objRef) ) ){
+            if( prop == propName || prop == "_all" ){
+              for( let funcRef of this.sub.get(objRef)[propName] ){
+                if( func == funcRef.name || func == funcRef || func == "_all" ){ // unsubscribe code
+
+                  if( func == "_all" )
+                    this.sub.get(objRef)[propName] = []
+                  else
+                    this.sub.get(objRef)[propName] = arrayWithout( this.sub.get(objRef)[propName], funcRef )
+
+                } // end of unsubscribe code
+              }
+            }
+          }
+        }
+      }
+    }.bind(this)() //close unsubscribe
+  },
+  arrayWithout: function ( arrayRef, itemRef ){
+    return arrayRef.filter( function(item){
+      return item !== itemRef
+    } )
+  }
 }
