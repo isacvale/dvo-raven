a7153e945347a304557ea88ef1e350d1293bee7d
isacvale <isac@ocastudios.com>
2019-09-12 01:41:12 -0300

    A working version ready for tests.

--- README.md
+++ README.md
@@ -1,5 +1,56 @@
-## Preparing your store
-Create a single object containing all your state. You may write it in different objects, but must assemble them in order to be loaded in raven.
+# Raven
+
+## Overview
+### What it does
+Raven is a way to keep state and do data binding. It was made for sites/apps developed in vanilla, without frameworks.
+
+### How to use it
+After calling raven.**load**() and passing it your app's state as a single object, you can call the following methods:
+1. **subscribe**(*obj*, *callback*) runs the given callback when the variables indicated by obj change.
+1. **set**(*obj*) is how you change the state of yout app. Changing it directly won't fire callbacks.
+1. **push**(*from*, *to*, *func*) makes it so every change to variable "from" is reflected up to state field "to", after passing through function "func".
+1. **pull**(*from*,*to*, *func*) makes it so every change to state field "from" is reflected down to variable "to", after passing through function "func".
+1. **sync**(*state*,*var*,*statefunc*,*varfunc*) makes it so any changes to store or variable are reflected on each other, having passed through a given function.
+
+---
+
+## How to reference state variables
+In Raven, you reference your state variables by passing an object that mirrors the "path" to that variable. For example, suppose you want to give a value to the variable "answer" in the state below:
+
+    state = {
+      universe: {
+        answer: null,
+        question: null
+      }
+    }
+
+You'd do:
+
+    raven.set({universe: {answer: 42}})
+
+So far, so simple. But suppose you want to *reference* the variable, and *not provide* the value. In this case, the name of the variable is passed as a string, and no value is passed. For example, to subscribe a callback for changes to variable "answer", you'd do:
+
+    raven.subscribe({universe: "answer"}, callbackFunction)
+
+The advantage using objets for referencing that you can point to multiple state properties at once. For example, if you wanted to subscribe to changes to both question and answer in the example above, you'd do:
+
+    raven.subscribe({universe: ["answer", "question"]}, callbackFunction)
+
+And there's still a third way to do references. If the variable you're after is not in the state, you must pass a reference to its parent object and it's property name. For example, to push up the value from input, you could do:
+
+    raven.push({
+        target: document.querySelector("#arthurs-brain"),
+        prop: "value"
+      },
+      { universe: "question" }
+    )
+
+Once you take in these referencing rules, using raven will be easy-peasy.
+
+---
+## A deeper view
+### Preparing your store
+Create a single object containing all your state. You may write it in different objects, but must assemble them in order to be loaded into raven.
 
 To do so, use the command raven.load(), as in the example below.
 
@@ -19,14 +70,13 @@ To do so, use the command raven.load(), as in the example below.
 
     raven.load(myState)
 
-## Writing the callbacks
-The callbacks contain much of the logic of the application. They tend to be very short functions that manipulate the DOM.
-For example,
+### Writing the callbacks
+The callbacks contain much of the logic of the application. They tend to be very short functions that manipulate the DOM. For example,
 
     const warnOfColorChange = () => document.querySelector('#warn-output')
     .textContent = "The color has changed!"
 
-## Registering callbacks
+### Registering callbacks
 When a callback is registered to a property of your state, that callback will fire whenever a change is observed on the target property.
 
 To register a callback, use raven.register() passing an object to represent which properties to watch and the callback function. In the example below, the callback will run whenever the favoriteColor is modified.
@@ -40,22 +90,22 @@ To register a callback, use raven.register() passing an object to represent whic
       callback
     )
     
-There are some important things to aware of:
+There are some important things to be aware of:
 1. the object argument has the name of the argument to listen **as a string**, so to watch "bar" in {foo: {bar: 42}}, you'd pass {foo: 'bar'}.
-1. Many variables to watch may be passed in a single csll. The callback would run whenever any of those change.
-1. The callback will receive two arguments, the new and the old 
+1. Many variables to watch may be passed in a single call. The callback would run whenever any of those change.
+1. The callback will receive as argument the new value.
 
-### Changing state on complex data
+#### Changing state on complex data
 You may associate callbacks to entire objects instead of its particular properties. This means a callback will fire if any of its properties are changed.
 Extending the example above, we could rig callback to fire when any dislikes changes by subscribing:
 
     raven.register({personal: "dislikes"}, callback)
 
-### Subscribing root properties
+#### Subscribing root properties
 Because you can't create a JS object with a key without value, subscribing a root property is a bit of a gotcha. There's two ways to do it:
 
 1. You can pass it as a string. For example raven.subscribe("personal", callback) would run whenever any personal prop changed.
-1. If you need to pass an object, you can just assign it any value other than an object or a string - we recommend 0. So raven.subscribe({personal: 0}, callback) would work just fine.
+1. If you need to pass an object, you can just assign it any value other than an object, array or a string - we recommend 0. So raven.subscribe({personal: 0}, callback) would work just fine.
 
 
 ## Changing state
@@ -69,5 +119,5 @@ For example, consider the call
       }
     })
 
-This will change the state in two positions, country and city, and all callbacks associated with both field will be executed.
+This will change the state in two positions, country and city, and all callbacks associated with both field will be executed. In fact, any callbacks rigged to 'location' would also fire (once).
 
--- raven.mjs
+++ raven.mjs
@@ -1,306 +1,256 @@
 const raven = {
 
-    /*
-    Allows the registration of the store, so it doesn't have to be supplied constantly.
-    */
-    store: {},
-    load( userStore ){
-        this.store = userStore
-    },
+  /*
+  Allows the registration of the store, so it doesn't have to be resupplied constantly.
+  */
+  store: {},
+  load( userStore ){
+  this.store = userStore
+  },
 
-    /*
-    An array will hold a reference to objects, arrays or scalars as a stringified array of prop keys.
-    It also holds the corresponding list of callback functions to call when it detects changes.
-    */
-    sub: {},
+  /*
+  An object will hold as keys a reference to objects, arrays or scalars. They keys are a stringified version of the object.
+  As values to those keys it holds a list of callback functions to call when it detects changes.
+  */
+  sub: {},
 
-    /*
-    A prefix for getter/setter
-    */
-    prefix: "_",
+  /*
+  A prefix for getter/setters.
+  */
+  prefix: "_",
 
-    /*
-    Util functions: get prop type
-    */
-    isArray (prop){
-        return Array.isArray( prop )
-    },
-    isObject (prop){
-        return typeof prop == "object" && !this.isArray( prop ) && prop != null
-    },
-    isScalar (prop){
-        return !isObject && !isArray
-    },
+  /*
+  These functions will detect the type of a prop so it can be dealt with properly.
+  */
+  isArray (prop){
+    return Array.isArray( prop )
+  },
+  isObject (prop){
+    return typeof prop == "object" && !this.isArray( prop ) && prop != null
+  },
 
-
-    /*
-    Gets an array of strings and uses them to reach inside an object to return a nested property.
-    */
-    reach (arr, obj=this.store) {
-        if(arr.length == 1)
-            return {target:obj, prop: arr[0]}
-        
-        const newArray = [...arr],
-              popped = newArray.shift(),
-              nextObj = obj[popped]
-        return this.reach(newArray, nextObj)
-    },
-
-    /*
-    Takes an object and returns an array of arrays, each containing the strings to make up the branches of the object.
-    */
-    treeToList ( obj ) {
-        let i=0
-        const original = []
-
-        const treeToListLoop = (obj, acc=[]) => {
-            if (this.isObject(obj))
-                Object.keys(obj).forEach( child =>
-                    treeToListLoop( obj[child], [...acc, child])
-                )
-            else if (typeof obj == "string")
-                original.push([...acc, obj])
-            else
-                original.push([...acc])
-        }    
-        treeToListLoop(obj)
-        return original
-    },
-    listToTree (list) {
-        const tree = {}
-
-        const listToTreeLoop = (path, curr, original) => {
-            path = [...path]
-            const newKey = path.shift()
-            if(path.length>1){
-              if ( !curr[newKey] ) {
-                curr[newKey] = {}                
-              }
-              return listToTreeLoop(path, curr[newKey], original)
-            }
-            else {
-              curr[newKey] = path.shift()
-              return original
-            }
-        }
-        list.forEach( path => {
-            listToTreeLoop(path, tree, tree)
-        })
+  /*
+  Gets an array of strings and uses them to reach inside an object to return a nested property.
+    Example:
+    Assume a state = {a: {b: 'c'}},
+    passing an argument ['a','b'] would
+    return the object { target: {b: 'c'}, prop: 'b' }.
+  */
+  reach (arr, obj=this.store) {
+    if(arr.length == 1)
+    return {target:obj, prop: arr[0]}
   
-        return tree
-    },
-    addValueToTree (obj, value) {
-        const listFromTree = this.treeToList(obj)
-        const mappedList = listFromTree.map( path => [...path, value])
-        const tree = this.listToTree([...mappedList])
-        return tree
-    },
+    const newArray = [...arr]
+    const popped = newArray.shift()
+    const nextObj = obj[popped]
+    return this.reach(newArray, nextObj)
+  },
 
-    /*
-    Subscribe allows a callback to be called whenever the prop changes on the given store.
-    */
-    subscribe ( tree, callback, thisStore=this.store ) {
-        const observedProps = this.treeToList(tree)
-        observedProps.forEach( list => {
-            const key = JSON.stringify(list)
-            if(!this.sub[key]){
-                this.sub[key] = []
-            }
-            this.sub[key].push(callback)
-        })
-    },
+  /*
+  Takes an object and returns an array of arrays, each containing the strings to make up the branches of the object.
+    Example:
+    Passing an object {a: {b: 1, c: 2}}
+    would return [[a,b], [a,c]]
+  */
+  treeToList ( obj ) {
+    let i=0
+    const original = []
 
-    /*
-    Gets [x,y,z] and returns [[x],[x,y],[x,y,z]]. Useful for traversing trees.
-    */
-    expandList (array) {
-        const expandedArray = []
-        array.forEach( (item, idx) => {
-            if( idx == 0 )
-                expandedArray.push( [item] )
-            else {
-                const newItem = [...expandedArray[idx-1]]
-                newItem.push(item)
-                expandedArray.push(newItem)
-            }
-        })
-        return expandedArray
-    },
-
-    /*
-    Uses getters and setters to make props reactive.
-    Relies on closure on sub.
-    Deprecated in favor of "set" method.
-    */
-//    reactToChange (list, store) {
-//         // console.log( "list:", list)
-//         // console.log( "reached:",  this.reach(list, store))
-//         const reached = this.reach(list, store)
-        
-//         Object.defineProperty(
-//             reached.target,
-//             `${this.prefix}${reached.prop}`,
-//             {
-//                 enumerable: false,
-//                 writable: true
-//             }
-//         )
-//         reached.target[`${this.prefix}${reached.prop}`]=reached.target[`${reached.prop}`]
-
-//         Object.defineProperty(
-//             reached.target,
-//             reached.prop,
-//             {
-//                 set: value => {
-//                     const callbackArgs = {
-//                         new: value,
-//                         old: reached.target[`${this.prefix}${reached.prop}`]
-//                     }
-//                     const expandedList = this.expandList(list)
-//                     expandedList.forEach(list => {
-//                         const key = JSON.stringify(list)
-//                         // console.log("key:", key)
-//                         if( this.sub[key] )
-//                             this.sub[key].forEach( callback => callback(callbackArgs) )
-//                     })
-//                     reached.target[`${this.prefix}${reached.prop}`]=value
-//                 },
-//                 get: () => reached.target[`${this.prefix}${reached.prop}`]
-//             }
-//         )
-//     },
-
-    /*
-    Set given state to store. If payload is an object or array.
-    */
-    // setOriginal (arg, store = this.store) {  
-    //     const setLoop = (obj, ref) => Object.keys(obj).forEach( key => {
-    //         // Check if it exists on store, otherwise create it
-    //         if(!ref[key]) ref[key] = undefined
-    //         // If it references an object, loop through its keys
-    //         if(this.isObject(obj[key]))
-    //             setLoop(obj[key], store[key])
-    //         // If it is an array, substitute it
-    //         else if(this.isArray(obj[key]))
-    //             ref[key] = [...obj[key]]
-    //         // If it is scalar, substitute it
-    //         else ref[key] = obj[key]
-    //     })
-    //     setLoop( arg, store)
-    //     this.doCallbacksOriginal( arg, store )
-    // },
-    set (arg, store = this.store) {
-        const listOfPaths = []
-        const setLoop = (obj, ref, path =[]) => Object.keys(obj).forEach( key => {
-            // Check if it exists on store, otherwise create it
-            if(!ref[key]) ref[key] = undefined
-            // If it references an object, loop through its keys
-            if(this.isObject(obj[key])) {
-                path.push(key)
-                setLoop(obj[key], store[key], path)
-            }
-            // If it is an array, substitute it
-            else if(this.isArray(obj[key])) {
-                if (ref[key] != [...obj[key]]) {
-                    listOfPaths.push([...path, key])
-                    ref[key] = [...obj[key]]
-                }
-                else console.log('was equal?')
-        }
-            // If it is scalar, substitute it
-            else if (ref[key] != obj[key]) {
-                listOfPaths.push([...path, key])
-                ref[key] = obj[key]
-            }
-            else console.log('was equal?')
-        })
-        setLoop( arg, store)
-        console.log('..listOfPaths', listOfPaths)
-        this.doCallbacks( listOfPaths, store )
-    },
-
-    /*
-    Initiate callback
-    */
-    // doCallbacksOriginal( arg, store=this.store) {
-    //     const listOfArgs = this.treeToList(arg)
-    //     const expandedList = listOfArgs.map( list => this.expandList(list))
-    //     expandedList.forEach( fullList => fullList.forEach( list => {
-    //         const key = JSON.stringify(list)
-    //         if( this.sub[key] ){
-    //             const reached = this.reach(list)
-    //             this.sub[key].forEach( func => func && func(reached.target[reached.prop]))
-    //         }
-    //     }))
-    // },
-
-    doCallbacks( listOfArgs, store=this.store) {
-        const expandedList = listOfArgs.map( list => this.expandList(list))
-        expandedList.forEach( fullList => fullList.forEach( list => {
-            const key = JSON.stringify(list)
-            if( this.sub[key] ){
-                const reached = this.reach(list)
-                this.sub[key].forEach( func => func && func(reached.target[reached.prop]))
-            }
-        }))
-    },
-
-    isSynced(obj1, obj2){
-        const value1 = obj1.target[obj1.prop]
-        const value2 = obj2.target[obj2.prop]
-        const callback2 = obj2.callback || (x => x)
-        if( callback2(value1) == value2 )
-            return true
-        return false
-    },
-
-    push(from, to, callback = x => x){
-        if( from.target instanceof HTMLElement ) {
-            let events = from.event || ['change', 'input']
-            if( typeof from.event == 'string')
-                events = [from.event]
-            events.forEach( event => 
-                from.target.addEventListener(event, e => {
-                    const callbackArg = e.target['value'] || e.target.innerHTML
-                    const callbackResult = callback(callbackArg)
-                    this.set(this.addValueToTree(to, callbackResult))
-                })
-            )
-        } else {
-            Object.defineProperty(
-                from.target,
-                from.prop,
-                {
-                    set: value => {
-                        const listOfPaths = this.treeToList(to).map( list => [...list, callback(value)])
-                        const newTree = this.listToTree(listOfPaths)
-                        raven.set(newTree)
-    
-                        from.target[`${this.prefix}${from.prop}`]=value
-                    },
-                    get: () => from.target[`${this.prefix}${from.prop}`]
-                }
-            )
-        }
-    },
-
-    pull(from, to, callback = x => x){
-        this.subscribe(from, value => to.target[to.prop] = callback(value))
-    },
-
-    sync(obj1, obj2){
-        this.pull(obj1, obj2, obj1.callback)
-        this.push(obj2, obj1, obj2.callback)
+    const treeToListLoop = (obj, acc=[]) => {
+      if (this.isObject(obj))
+        Object.keys(obj).forEach( child =>
+          treeToListLoop( obj[child], [...acc, child]))
+      else if (this.isArray(obj))
+          obj.forEach(child => treeToListLoop( obj[child], [...acc, child]))
+      else if (typeof obj == "string")
+        original.push([...acc, obj])
+      else
+        original.push([...acc])
     }
 
+    treeToListLoop(obj)
+    return original
+  },
 
-    // const isPropSubscribed = (sub, prop, callback) => {
-    //     const children = Object.keys( prop )
-    //     children.forEach( child => {
-    //         if(isScalar)
-    //             sub[child] ? sub[child].push(child) : sub[child] = [child]
-    //     })
-    // }
+  /*
+  Opposite to treeToList(), this function will assemble a list of prop names into an object.
+    Example:
+    Passing an object [[a,b], [a,c]]
+    would return {a: {b: 1, c: 2}}
+  */
+
+  listToTree (list) {
+    const tree = {}
+
+    const listToTreeLoop = (path, curr, original) => {
+      path = [...path]
+      const newKey = path.shift()
+      if (path.length>1) {
+        if (!curr[newKey]) {
+          curr[newKey] = {}                
+        }
+      return listToTreeLoop(path, curr[newKey], original)
+      } else {
+        curr[newKey] = path.shift()
+        return original
+      }
+    }
+    list.forEach( path => {
+      listToTreeLoop(path, tree, tree)
+    })
+    
+    return tree
+  },
+
+  /*
+    Takes an object that represents the branches of the tree and add the to each branches.
+      Example:
+      Assume a state of {b: {c: 2, d: 3}},
+      passing addValueToTree({b: 'c'}, 4)
+      will change the state to {a: 1, b: {c: 4, d: 3}}.
+  */
+  addValueToTree (obj, value) {
+    const listFromTree = this.treeToList(obj)
+    const mappedList = listFromTree.map( path => [...path, value])
+    const tree = this.listToTree([...mappedList])
+    return tree
+  },
+
+  /*
+  Subscribe allows a callback to be called whenever the prop changes on the given store.
+  */
+  subscribe ( tree, callback, thisStore=this.store ) {
+    const observedProps = this.treeToList(tree)
+    observedProps.forEach( list => {
+      const key = list.join('|')
+      // const key = JSON.stringify(list)
+      if(!this.sub[key]){
+        this.sub[key] = []
+      }
+      this.sub[key].push(callback)
+    })
+  },
+
+  /*
+  Gets [x,y,z] and returns [[x],[x,y],[x,y,z]]. Useful for traversing trees.
+  */
+  expandList (array) {
+    const expandedArray = []
+    array.forEach( (item, idx) => {
+      if( idx == 0 )
+      expandedArray.push( [item] )
+      else {
+        const newItem = [...expandedArray[idx-1]]
+        newItem.push(item)
+        expandedArray.push(newItem)
+      }
+    })  
+    return expandedArray
+  },
+
+  /*
+  The primary way of changing values in the state. When state is changed via "set", the callbacks get properly activated.
+  */
+  set (arg, store = this.store) {
+    const listOfPaths = []
+    const setLoop = (obj, ref, path =[]) => Object.keys(obj).forEach( key => {
+    // Check if it exists on store, otherwise create it
+    if(!ref[key]) ref[key] = undefined
+    // If it references an object, loop through its keys
+    if(this.isObject(obj[key])) {
+      path.push(key)
+      setLoop(obj[key], store[key], path)
+    }
+    // If it is an array, substitute it
+    else if(this.isArray(obj[key])) {
+    if (ref[key] != [...obj[key]]) {
+      listOfPaths.push([...path, key])
+      ref[key] = [...obj[key]]
+    } else console.log('was equal?')
+    }
+    // If it is scalar, substitute it
+    else if (ref[key] != obj[key]) {
+      listOfPaths.push([...path, key])
+      ref[key] = obj[key]
+    }
+    else console.log('was equal?')
+  })
+    setLoop( arg, store)
+    console.log('..listOfPaths', listOfPaths)
+    this.doCallbacks( listOfPaths, store )
+  },
+
+  /*
+  Runs callbacks subscribed under the arguments.
+    Example:
+    If ['country', 'state', 'town'] is passed as argument, all callbacks registered
+    under "country", "country|state" and "country|state|town" get called.
+  */
+  doCallbacks( listOfArgs, store=this.store) {
+    const expandedListRepeated = listOfArgs.map( list => this.expandList(list))
+    const expandedList = [...new Set(expandedListRepeated)]
+    expandedList.forEach( fullList => fullList.forEach( list => {
+      const key = list.join('|')
+      // const key = JSON.stringify(list)
+      if( this.sub[key] ){
+        const reached = this.reach(list)
+      this.sub[key].forEach( func => func && func(reached.target[reached.prop]))
+    }
+    }))
+  },
+
+  /*
+  Hooks up so that any change to a variable "from" is send through "callback"
+  and set to state field "to". It is mainly meant from collecting input from user
+  and automatically setting it to state. 
+  */
+  push(from, to, callback = x => x){
+    if( from.target instanceof HTMLElement ) {
+      let events = from.event || ['change', 'input']
+      if( typeof from.event == 'string')
+        events = [from.event]
+      events.forEach( event => 
+        from.target.addEventListener(event, e => {
+          const callbackArg = e.target['value'] || e.target.innerHTML
+          const callbackResult = callback(callbackArg)
+          this.set(this.addValueToTree(to, callbackResult))
+        })
+      )
+    } else {
+      Object.defineProperty(
+        from.target,
+        from.prop,
+        {
+          set: value => {
+          const listOfPaths = this.treeToList(to).map( list => [...list, callback(value)])
+          const newTree = this.listToTree(listOfPaths)
+          raven.set(newTree)
+        
+          from.target[`${this.prefix}${from.prop}`]=value
+          },
+          get: () => from.target[`${this.prefix}${from.prop}`]
+        }
+      )
+    }
+  },
+
+  /*
+  Hooks up so that any change to state field "from" goes through "callback"
+  and is reflected to variable "to". It is mainly meant for data binding.
+  */
+  pull(from, to, callback = x => x){
+    this.subscribe(from, value => to.target[to.prop] = callback(value))
+  },
+
+  /*
+  A combination of push and pull. It allows change in a variable
+  (let's say, an input) to be pushed up to store, and any changes to store
+  to be passed down.
+  */
+  sync(stateTree, obj, callbackToTree, callbackToObject){
+    this.pull(stateTree, obj, callbackToObject)
+    this.push(obj, stateTree, callbackToTree)
+  }
 } 
 
 export default raven
